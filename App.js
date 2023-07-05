4a97e98cf5d2764ba3eb54d3140586f9c8d3cfe3
EdisonLopezEC <edisonlopezec@gmail.com>
2023-07-04 23:14:06 -0500

    Login Screen and Functions

--- App.js
+++ App.js
@@ -2,11 +2,7 @@ import React from "react";
 import {  Text} from "react-native";
 // import { NavigationContainer} from '@react-navigation/native';
 
-
-
-
 export default function App() {
-  
   return (
     
    <Text>
