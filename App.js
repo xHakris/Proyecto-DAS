1bf5cd8ffce921fcc2c4abc2c0d3a91dc8b71beb
EdisonLopezEC <edisonlopezec@gmail.com>
2023-07-04 22:41:17 -0500

    Inicio del proyecto

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {  Text} from "react-native";
+// import { NavigationContainer} from '@react-navigation/native';
+
+
+
+
+export default function App() {
+  
+  return (
+    
+   <Text>
+    Inicio del proyecto
+   </Text>
+  );
+}
