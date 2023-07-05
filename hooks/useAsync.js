b027b1fafada82b07f75d20a8006fcf6be3e5662
Mariela Amanta <mamanta4438@uta.edu.ec>
2023-07-05 00:45:55 -0500

    SD-37 Hook use local storage add for login

--- hooks/useAsync.js
+++ hooks/useAsync.js
@@ -27,4 +27,17 @@ export const useAsync = (asyncFn, onSuccess)=> {
   
     return debouncedValue;
   };
+
+  export const useLocalStorage = (key, initialValue) => {
+    const [value, setValue] = useState(() => {
+      const storedValue = localStorage.getItem(key);
+      return storedValue ? JSON.parse(storedValue) : initialValue;
+    });
+  
+    useEffect(() => {
+      localStorage.setItem(key, JSON.stringify(value));
+    }, [key, value]);
+  
+    return [value, setValue];
+  };
   
