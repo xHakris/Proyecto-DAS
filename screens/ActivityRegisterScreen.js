1b3bdac27fc38520044ea3a465bd37ade66bf608
EdisonLopezEC <edisonlopezec@gmail.com>
2023-07-05 21:13:04 -0500

    SD-38 New Page add for activity

---
+++ screens/ActivityRegisterScreen.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react";
+import {
+  Text,
+} from "react-native";
+
+const ActivityRegisterScreen = () => {
+
+  return (
+    <Text>
+      Nueva pagina para el registro
+    </Text>
+  );
+};
+
+export default ActivityRegisterScreen;
